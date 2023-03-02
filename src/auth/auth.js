4caad5470f36b0e629e912e704195f71a8996912
Victor Grossi <victoraugustogrossi@gmail.com>
2023-03-01 22:02:30 -0300

    feat: add authentication to router

---
+++ src/auth/auth.js
@@ -0,0 +1,20 @@
+const EXPIRATION_IN_MINUTES = 5;
+
+export function isAuthenticated()  {
+    const user = JSON.parse(
+        localStorage.getItem(
+            localStorage.getItem('loggedUser')
+        )
+    )
+
+    const navigationStart = new Date(user?.token);
+    const timeElapsed = Math.round((Date.now() - navigationStart) / 1000)
+    if(timeElapsed > (EXPIRATION_IN_MINUTES * 60)) {
+        // TODO warn user that his login has expired
+        console.log('TOKEN EXPIRADO')
+        return false
+    }
+    return true;
+}
+
+// TODO Create time tokens to expire login session
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { isAuthenticated } from '../auth/auth'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginView,
+      meta: { requiresAuth: false }
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/signup',
+      name: 'signup',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../views/SignupView.vue'),
+      meta: { requiresAuth: false }
+    } // TODO -> Create route for dashboard and authenticated;
   ]
 })
+router.beforeEach((to, from) => {
+  // instead of having to check every route record with
+  // to.matched.some(record => record.meta.requiresAuth)
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      name: 'login',
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath },
+    }
+  }
+})
+
 
 export default router

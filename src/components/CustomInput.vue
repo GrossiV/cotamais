52bb4ec92fd33ac726c6d99f3400ddb12a36c0a4
Victor Grossi <victoraugustogrossi@gmail.com>
2023-03-01 22:05:15 -0300

    feat: add error effect on input

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -9,29 +9,40 @@ defineProps({
   placeholder: {
     type: String,
     default: ''
+  },
+  hasError: {
+    type: Boolean,
+    default: false
   }
 })
 
-const emit = defineEmits(['onInputChange'])
+const emit = defineEmits(['onInputChange', 'onInputFocus'])
 
 const input = ref('')
 
 function updateInput(e) {
   emit('onInputChange', input.value)
 }
+function onFocus() {
+  emit('onInputFocus')
+}
 
 </script>
 
 <template>
-    <input class="input" @input="updateInput" v-model="input" :placeholder="placeholder" :type="type">
+    <input class="input" :class="{'input--error': hasError}" @focus="onFocus" @input="updateInput" v-model="input" :placeholder="placeholder" :type="type">
 </template>
 
 
 <style scoped>
 .input {
-  padding: 4px;
-  border: 1px solid #222;
-  border-radius: 2px;
+  padding: 6px;
+  border: 1px solid rgb(8, 85, 66);
+  border-radius: 14px;
+  text-indent: 14px;
+}
+.input--error {
+  border: 1px solid red;
 }
 .input:focus {
   outline: none;

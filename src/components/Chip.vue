5fd7cb59495c6f285e63f06fd28d9d3f871d97a8
Victor Grossi <victoraugustogrossi@gmail.com>
2023-03-04 17:56:12 -0300

    feat: add chip component

---
+++ src/components/Chip.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: ''
+  },
+  text: {
+    type: String,
+    default: '-0,025 %'
+  },
+})
+</script>
+
+<template>
+    <span class="chip" :class="`chip--${type}`">{{ text }}</span>
+</template>
+
+
+<style scoped>
+.chip {
+    padding: 0;
+    border-radius: 10px;
+    font-size: .65rem;
+    padding: 2px 6px;
+    color: #fff;
+    font-weight: bold;
+}
+.chip--success {
+    background-color: #0ACF97;
+}
+.chip--danger {
+    background-color: #FA5C7C;
+}
+</style>

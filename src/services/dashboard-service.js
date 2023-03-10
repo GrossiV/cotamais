c41019fe07a9e580ada1c2c0d094058cce6c8fb4
Victor Grossi <victoraugustogrossi@gmail.com>
2023-03-10 14:06:38 -0300

    fix: add key to environment variable

--- src/services/dashboard-service.js
+++ src/services/dashboard-service.js
@@ -1,6 +1,8 @@
 export function getStocksAndCurrencies() {
   return fetch(
-    'https://api.hgbrasil.com/finance?format=json-cors&fields=only_results,currencies,stocks&key=4f838463'
+    `https://api.hgbrasil.com/finance?format=json-cors&fields=only_results,currencies,stocks&key=${
+      import.meta.env.VITE_HGF_KEY
+    }`
   ).then((response) => {
     if (response.ok) {
       return response.json()

2ce7fe598119b4165db1692a6be902f7d707bf49
Victor Grossi <victoraugustogrossi@gmail.com>
2023-03-10 14:12:44 -0300

    fix: remove envronment variable

--- src/services/dashboard-service.js
+++ src/services/dashboard-service.js
@@ -1,8 +1,6 @@
 export function getStocksAndCurrencies() {
   return fetch(
-    `https://api.hgbrasil.com/finance?format=json-cors&fields=only_results,currencies,stocks&key=${
-      import.meta.env.VITE_HGF_KEY
-    }`
+    'https://api.hgbrasil.com/finance?format=json-cors&fields=only_results,currencies,stocks&key=4f838463'
   ).then((response) => {
     if (response.ok) {
       return response.json()
